c18e9f22dbeedd6708ae9ce04e609a71b0693a68
LEEDOWON96 <leedowon19@gmail.com>
2023-01-27 07:58:22 +0000

    Added s3 bucket private module

--- main.tf
+++ main.tf
@@ -202,6 +202,6 @@ resource "aws_key_pair" "hashicat" {
 }
 
 module "s3_bucket" {
-  source = "LEEDOWON96/tf-workshop-dw/s3-bucket/aws"
+  source  = "app.terraform.io/tf-workshop-dw/s3-bucket/aws"
   version = "3.0.0"
 }
